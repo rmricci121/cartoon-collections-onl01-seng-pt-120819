1f6e6a759cd8ed61c7a1d7e08edd7804c10ba933
Robert M Ricci <rmricci121@gmail.com>
2019-12-15 10:26:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,9 @@ def summon_captain_planet(planateer_calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.any? do |
+  calls.any? do |word|
+    word.length > 4
+  end
 end
 
 def find_the_cheese# code an argument here
