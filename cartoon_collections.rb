a678768f180adab9adc1a7eacacf896599a96af3
Robert M Ricci <rmricci121@gmail.com>
2019-12-15 10:28:48 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,12 @@ def long_planeteer_calls(calls)
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  if strings.find == cheese_types
+    return cheese_types
+  else
+    return nil 
+  end
 end
