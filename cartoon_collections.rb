f1e2de7451b1fc19731a49eb6acca6c9f08063f0
Robert M Ricci <rmricci121@gmail.com>
2019-12-15 10:31:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,5 +20,6 @@ def find_the_cheese(strings)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   strings.include do |cheese|
-    
+    cheese == cheese_types
+  end
 end
